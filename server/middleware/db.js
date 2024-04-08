5028bfebc6833091f43166365504fba38e446bf2
Ritik Tiwari <dev.ritiktiwari@gmail.com>
2024-04-08 22:53:09 +0530

    FIX: Mongoose error

--- server/middleware/db.js
+++ server/middleware/db.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
 const connectToDB = handler => async (req, res) => {
-    if (mongoose.connections[0].readyState) {
+    try {
+        if (mongoose.connections[0].readyState) {
+            return handler(req, res);
+        }
+
+        mongoose.set('strictQuery', false);
+        mongoose.connect(process.env.MONGO_URI).then((data) => {
+            console.log(`MongoDB Connected: ${data.connection.host}`);
+        }).catch((err) => {
+            console.log(err);
+        });
+
         return handler(req, res);
-    }
-    
-    mongoose.set('strictQuery', false);
-
-    mongoose.connect(process.env.MONGO_URI).then((data) => {
-        console.log(`MongoDB Connected: ${data.connection.host}`);
-    }).catch((err) => {
+    } catch (err) {
         console.log(err);
-    });
-
-    return handler(req, res);
+    }
 };
 
 export default connectToDB;
